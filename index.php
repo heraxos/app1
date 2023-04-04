f815e2414b8e10b939a608f27851b1e0b3d694ef
Armen <91148484+heraxos@users.noreply.github.com>
2023-04-04 09:11:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ $user = 'u47755';
 $pass = '2914865';
 $db = new PDO('mysql:host=localhost;dbname=u47755', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
 try {
-  $stmt = $db->prepare("INSERT INTO bentex SET name=:name, email=:email, birthdate=:birthdate, sex=:sex, limb_count=:limbs, bio=:bio");
+  $stmt = $db->prepare("INSERT INTO tabel1 SET name=:name, email=:email, birthdate=:birthdate, sex=:sex, limb_count=:limbs, bio=:bio");
   $stmt->bindParam(':name', $name);
   $stmt->bindParam(':email', $email);
   $stmt->bindParam(':birthdate', $birth);
@@ -82,7 +82,7 @@ try {
   exit();
   }
   $id = $db->lastInsertId();
-  $sppe= $db->prepare("INSERT INTO bentex SET name=:name, person_id=:person");
+  $sppe= $db->prepare("INSERT INTO superpowers SET name=:name, person_id=:person");
   $stmt->bindParam(':email', $email);
   $sppe->bindParam(':person', $id);
   foreach($superpowers as $inserting){
