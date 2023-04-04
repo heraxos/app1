989024ab373a39d5e073d7346d179765aa4a004a
Armen <91148484+heraxos@users.noreply.github.com>
2023-04-04 09:07:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,80 +1,55 @@
-<?php
-// Отправляем браузеру правильную кодировку,
-// файл index.php должен быть в кодировке UTF-8 без BOM.
-header('Content-Type: text/html; charset=UTF-8');
-
-// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
-// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
-  if (!empty($_GET['save'])) {
-    // Если есть параметр save, то выводим сообщение пользователю.
-    print('Спасибо, результаты сохранены.');
-  }
-  // Включаем содержимое файла form.php.
-  include('form.php');
-  // Завершаем работу скрипта.
-  exit();
-}
-// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
-
-// Проверяем ошибки.
-$errors = FALSE;
-if (empty($_POST['fio'])) {
-  print('Заполните имя.<br/>');
-  $errors = TRUE;
-}
-
-if (empty($_POST['year']) || !is_numeric($_POST['year']) || !preg_match('/^\d+$/', $_POST['year'])) {
-  print('Заполните год.<br/>');
-  $errors = TRUE;
-}
-
-
-// *************
-// Тут необходимо проверить правильность заполнения всех остальных полей.
-// *************
-
-if ($errors) {
-  // При наличии ошибок завершаем работу скрипта.
-  exit();
-}
-
-// Сохранение в базу данных.
-
-$user = 'u47755'; // Заменить на ваш логин uXXXXX
-$pass = '2914865'; // Заменить на пароль, такой же, как от SSH
-$db = new PDO('mysql:host=localhost;dbname=u47755', $user, $pass,
-  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
-
-// Подготовленный запрос. Не именованные метки.
-try {
-  $stmt = $db->prepare("INSERT INTO armo SET name = ?");
-  $stmt->execute([$_POST['fio']]);
-}
-catch(PDOException $e){
-  print('Error : ' . $e->getMessage());
-  exit();
-}
-
-//  stmt - это "дескриптор состояния".
- 
-//  Именованные метки.
-//$stmt = $db->prepare("INSERT INTO test (label,color) VALUES (:label,:color)");
-//$stmt -> execute(['label'=>'perfect', 'color'=>'green']);
- 
-//Еще вариант
-/*$stmt = $db->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
-$stmt->bindParam(':firstname', $firstname);
-$stmt->bindParam(':lastname', $lastname);
-$stmt->bindParam(':email', $email);
-$firstname = "John";
-$lastname = "Smith";
-$email = "john@test.com";
-$stmt->execute();
-*/
-
-// Делаем перенаправление.
-// Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
-// Если ошибок при этом не видно, то необходимо настроить параметр display_errors для PHP.
-header('Location: ?save=1');
+<form action ="" method = "POST">
+	<label>
+			Name:<br />
+			<input name="field-name-1" />
+		  </label><br />
+	<label>
+			email:<br />
+			<input name="field-email"
+			  type="email" />
+		  </label><br />
+	<label>
+			Birth-date :<br />
+			<input name="field-date"
+			  type="date" />
+		  </label><br />
+	Sex: <label><input type="radio" checked="checked"
+			name="radio-group-1" value = "male" />
+			Male </label>
+		  <label><input type="radio"
+			name="radio-group-1" value = "female" />
+			Female </label><br />
+	Amoflimbs: <br/> <label><input type="radio" checked="checked"
+			name="radio-group-2" value = "1" />
+			1 </label><br/>
+		  <label><input type="radio"
+			name="radio-group-2" value = "2" />
+			2 </label><br />
+			<label><input type="radio"
+			name="radio-group-2" value = "3" />
+			3 </label><br />
+			<label><input type="radio"
+			name="radio-group-2" value = "4" />
+			4 </label><br />
+			<label><input type="radio"
+			name="radio-group-2" value = "5" />
+			5 </label><br />
+	<label>
+			Superpowers:
+			<br/>
+			<select name="field-name-4[]"
+			  multiple="multiple">
+			  <option value="immortality">Immortality</option>
+			  <option value="walkthroughwalls" >Walk through walls</option>
+			  <option value="levitation">levitation</option>
+			</select>
+		  </label><br />
+	<label>
+	Biography: <br/>
+	  <textarea name = "bio-field"> </textarea>
+	</label> <br/>
+	<label>
+	  <input type = "checkbox" name = "checkbox" /> </label> <br/>
+	  Send: <br/>
+	  <input type="submit" value="Sending" />
+  </form>
